40cd0039754e8769941cedfe21bc388880b23e0f
bhossa <bhossa@gmail.com>
2021-02-22 04:14:43 +0000

    Test submit 1

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,8 +33,13 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
-};
+   console.log("Let's play some Scrabble!");
+  word = input.question("\n\nEnter a word to score: ");
+  console.log(oldScrabbleScorer(word));
+  
+  };
+
+  
 
 let simpleScore;
 
