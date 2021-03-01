9b6f9316664d695350a79d3bac1c2ffd6fa02e61
bhossa <bhossa@gmail.com>
2021-03-01 01:44:40 +0000

    trying to clear scoringFunction undefined error

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -115,7 +115,7 @@ const scoringAlgorithms = [
 {
   name: "Scrabble",
   description: "The traditional scoring algorithm.",
-  scorerFunction: oldScrabbleScorer
+  scoringFunction: oldScrabbleScorer
 },
 
 ];
